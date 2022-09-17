81dded88c998403fd73f0cb199c8a2769a58db81
Slim Nate <slimnatekc@gmail.com>
2022-09-16 20:14:15 -0500

    add scaffolding of ImageEditor component, currently only with a dialog to create/open a project

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import PersistentAppDrawer from './components/PersistentAppDrawer';
 import { useState } from 'react';
+import ImageEditor from './components/ImageEditor';
 
 function App() {
   const [project, setProject] = useState(window.testProject);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <PersistentAppDrawer project={project}>
-      
+      <ImageEditor project={ project.images[selectedImage] || null } />
     </PersistentAppDrawer>
   );
 }
---
+++ src/components/ImageEditor.js
@@ -0,0 +1,24 @@
+import { Button, Paper, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+
+export default function ImageEditor({ image }) {
+    return (
+    <Box flexGrow={1} alignContent='center' alignItems='center'>
+        <Paper
+            elevation={4}
+            sx={{
+                width: '300px',
+                alignContent: 'center',
+                flexGrow: 1
+            }}
+        >
+            <Stack direction='column' alignItems='center' padding='20px'>
+                <Typography>No project currently open, please:</Typography>
+                <Button variant='contained'>Create project from folder</Button>
+                <Typography variant='subtitle2'>OR</Typography>
+                <Button variant='contained'>Open existing project</Button>
+            </Stack>
+        </Paper>
+    </Box>
+    );
+}
