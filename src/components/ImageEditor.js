93f225ce00d5dd2200443a7c0674d974dea02429
Slim Nate <slimnatekc@gmail.com>
2022-09-21 20:03:38 -0500

    Add keep/discard buttons to image editor component

--- src/components/ImageEditor.js
+++ src/components/ImageEditor.js
@@ -1,7 +1,8 @@
-import { Button, Paper, styled, Typography } from "@mui/material";
+import { Check, Clear } from "@mui/icons-material";
+import { Button, ButtonGroup, Paper, styled, Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 
-const ImageHolder = styled(Paper)(({theme}) => ({
+const ImageHolder = styled(Paper)(() => ({
     flexGrow: 1,
     flexShrink: 1,
     marginTop: '48px',
@@ -11,30 +12,45 @@ const ImageHolder = styled(Paper)(({theme}) => ({
     alignItems: 'center',
 }));
 
-const Image = styled('img')(({theme}) => ({
+const Image = styled('img')(() => ({
     maxWidth: '100%',
     maxHeight: '100%',
 }));
 
-const MainContainer = styled(Box)(({theme}) => ({
+const MainContainer = styled(Box)(() => ({
     display: 'flex',
     flexGrow: 1,
     flexDirection: 'column',
+    justifyContent:'stretch',
+}));
+
+const KeepButton = styled(Button)(() => ({
+    flexGrow: '1',
 }));
 
 export default function ImageEditor({ projectExists, image }) {
     if (projectExists) {
         return (
             <MainContainer>
-                <ImageHolder container>
+                <ImageHolder>
                     <Image src={`atom://${image.path}`} alt='Currently selected, full size'></Image>
                 </ImageHolder>
                 <Paper sx={{
                     flexGrow: 1,
-                    padding: '10px'
+                    display: 'flex',
+                    paddingBottom: '10px',
+                    height: '40px',
+                    justifyContent: 'center',
+                    alignItems: 'flex-end',
                 }}>
-                        Image buttons<br/>
-                        Image buttons<br/>
+                    <ButtonGroup>
+                        <KeepButton variant='contained' color='error'>
+                            <Clear></Clear>
+                        </KeepButton>
+                        <KeepButton variant='contained' color='success'>
+                            <Check></Check>
+                        </KeepButton>
+                    </ButtonGroup>
                 </Paper>
             </MainContainer>
         );
