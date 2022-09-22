cfde4321e95b2d24cca28bab905d54207eb83b76
Slim Nate <slimnatekc@gmail.com>
2022-09-21 19:11:48 -0500

    add more test images

--- public/preload.js
+++ public/preload.js
@@ -5,10 +5,13 @@ const textBasePath = path.join(__dirname, '../')
 
 const testImageCount = 15
 const testImages = [...Array(testImageCount).keys()].map((img, i) => {
+    const mod = i % 6;
+    let relPath = `test${mod}.png`;
+    
     return {
         id: i,
-        relativePath: 'logo512.png',
-        path: path.join(__dirname, 'logo512.png'),
+        relativePath: relPath,
+        path: path.join(__dirname, relPath),
         keep: false,
     }
 })
